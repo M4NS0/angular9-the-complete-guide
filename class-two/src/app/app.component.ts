3a6a33dddd156f477eed5d97057e9c9e4a333ebb
Bruno C. Manso <mrbrunomanso@gmail.com>
2021-04-06 10:30:25 -0300

    Adding styles inside app.component.ts's annotation

--- class-two/src/app/app.component.ts
+++ class-two/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue;
+    font-weight: 800;
+  }`]
 })
 export class AppComponent {
   title = 'test';
