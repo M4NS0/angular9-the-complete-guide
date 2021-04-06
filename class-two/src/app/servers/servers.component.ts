df2fa58f5ba7f4edd6937efbfad84f69a506f812
Bruno C. Manso <mrbrunomanso@gmail.com>
2021-04-06 10:01:43 -0300

    Adding a DOM inside annotation's template's attribute

--- class-two/src/app/servers/servers.component.ts
+++ class-two/src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `<app-server></app-server><p> is inside the annotation's template attributes</p>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
